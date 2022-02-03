34c6d023bed2120c8c16e622b734f019b852d1ec
“Gabriela <gabriela_w@dotcubed.io>
2022-02-03 20:38:27 +0000

    remove store function from PostController

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -31,16 +31,7 @@ class PostController extends Controller
 
     }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
-    {
-        //
-    }
+
 
     /**
      * Display the specified resource.
